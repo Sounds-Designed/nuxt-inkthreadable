eb325267935ef0022cda4e0a2baac8e3a2744e00
Alex Scott <codetheorist@outlook.com>
2026-02-06 18:21:35 +0000

    feat: add test data to submit order function

--- playground/app/pages/orders.vue
+++ playground/app/pages/orders.vue
@@ -18,7 +18,39 @@
   const submitOrder = () => {
     console.log("Submitting Order");
 
-    fetch("/api/_inkthreadable/orders", { method: "POST" });
+    fetch("/api/_inkthreadable/orders", {
+      method: "POST",
+      body: JSON.stringify({
+        brandName: "Sounds Designed",
+        comment: "Test order.",
+        shipping_address: {
+          firstName: "Alex",
+          lastName: "Scott",
+          company: "Sounds Designed",
+          address1: "37 Slatey Road",
+          address2: "",
+          city: "Oxton",
+          county: "Merseyside",
+          postcode: "CH43 4UE",
+          country: "United Kingdom",
+          phone2: "+44 (0)7368 113827",
+        },
+        shipping: { shippingMethod: "courier" },
+        items: [
+          {
+            pn: "JH001",
+            quantity: 1,
+            retailPrice: 50,
+            description: "Please print as large as posible",
+            label: { type: "printed", name: "system-1 top gal" },
+            designs: {
+              front: "http://animalfair.com/wp-content/uploads/2014/06/little_cute_cat_1920x1080.jpg",
+              back: "http://data3.whicdn.com/images/168204223/large.jpg",
+            },
+          },
+        ],
+      }),
+    });
   };
 
   const deleteOrder = (orderId: string | number) => {
