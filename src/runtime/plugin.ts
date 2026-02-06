8d37dc141998a32a0034eb99369fa738247de2e0
Alex Scott <codetheorist@outlook.com>
2026-02-06 17:37:50 +0000

    feat: debug log plugin options in plugin

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,14 @@
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin, useRuntimeConfig } from "#app"
 
 export default defineNuxtPlugin((_nuxtApp) => {
-  console.log('Inkthreadable plugin initialized successfully!')
+  const config = useRuntimeConfig()
+
+  if(config.public.inkthreadable.debug) {
+    console.log('Inkthreadable plugin initialized successfully: \n%o', {
+      appId: config.inkthreadable?.appId,
+      secretKey: config.inkthreadable?.secretKey,
+      debug: config.public.inkthreadable?.debug,
+      enabled: config.public.inkthreadable?.enabled,
+    })
+  }
 })
