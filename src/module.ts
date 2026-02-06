237fe6b2f2102ce162215fe9937c3d081476cbd9
Alex Scott <codetheorist@outlook.com>
2026-02-06 17:35:39 +0000

    feat: module options

--- src/module.ts
+++ src/module.ts
@@ -1,20 +1,33 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { addPlugin, addServerHandler, createResolver, defineNuxtModule } from "@nuxt/kit";
+import { defu } from "defu";
+import type { ModuleOptions } from "nuxt/schema";
 
 // Module options TypeScript interface definition
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface ModuleOptions {}
+export interface InkthreadableOptions extends ModuleOptions {
+  appId: string;
+  debug?: boolean;
+  enabled?: boolean;
+  secretKey: string;
+}
 
-export default defineNuxtModule<ModuleOptions>({
-  meta: {
-    name: '@sounds-designed/nuxt-inkthreadable',
-    configKey: 'inkthreadable',
-  },
+export default defineNuxtModule<InkthreadableOptions>({
+  meta: { name: "@sounds-designed/nuxt-inkthreadable", configKey: "inkthreadable" },
   // Default configuration options of the Nuxt module
-  defaults: {},
+  defaults: { appId: "", debug: false, enabled: true, secretKey: "" },
   setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+    const resolver = createResolver(import.meta.url);
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    if (_options.enabled) {
+      _nuxt.options.runtimeConfig.inkthreadable = defu(_nuxt.options.runtimeConfig.inkthreadable, {
+        appId: _options.appId,
+        secretKey: _options.secretKey,
+      });
+
+      _nuxt.options.runtimeConfig.public.inkthreadable = defu(_nuxt.options.runtimeConfig.public.inkthreadable, {
+        debug: _options.debug,
+        enabled: _options.enabled,
+      });
+    }
   },
-})
+});
