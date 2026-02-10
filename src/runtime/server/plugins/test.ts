9f59ff9e635853de7737ae61ea685a072d6be925
Alex Scott <codetheorist@outlook.com>
2026-02-10 19:04:29 +0000

    feat: demo server plugin

---
+++ src/runtime/server/plugins/test.ts
@@ -0,0 +1,17 @@
+export default defineNitroPlugin(nitroApp => {
+  console.log("Nitro plugin", nitroApp);
+
+  nitroApp.hooks.hook("request", event => {
+    console.log("on request", event.path);
+  });
+
+  nitroApp.hooks.hook("render:response", (response, { event }) => {
+    console.log("on response", event.path, { response });
+
+    return response
+  });
+
+  nitroApp.hooks.hook("afterResponse", (event, { body }) => {
+    console.log("on after response", event.path, { body });
+  });
+});
