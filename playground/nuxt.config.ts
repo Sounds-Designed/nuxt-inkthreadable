941c057e105ebba07e295fc3a39a905683ea32eb
Alex Scott <codetheorist@outlook.com>
2026-02-10 17:54:11 +0000

    feat: playground config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: '2026-02-10',
   inkthreadable: {
+    apiEnabled: true,
     debug: true,
   },
+  runtimeConfig: { public: { inkthreadable: { debug: true } } },
 })
